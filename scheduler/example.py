59fb207fcd9aabca4272d10dfe5bf94b56a3e3f9
Chase Clarke <42917423+chsclarke@users.noreply.github.com>
2020-03-03 20:23:58 -0500

    Update example.py

--- scheduler/example.py
+++ scheduler/example.py
@@ -5,13 +5,14 @@ def myfunc():
     print("in job...")
 
 scheduler = Scheduler()
-id = scheduler.create_job(myfunc, seconds=1)
 
+id = scheduler.create_job(myfunc, seconds=1)
 print(">>> created job", id)
 
 time.sleep(3)
 
 print(scheduler.list_jobs())
+
 time.sleep(5)
 
 scheduler.remove_job(id)
