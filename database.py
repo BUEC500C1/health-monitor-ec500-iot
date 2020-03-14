691af7a54384c14b2e4f313886b34e509661154f
david <dlhi@bu.edu>
2020-03-14 15:32:49 -0400

    Added delete item for patientid

--- database.py
+++ database.py
@@ -44,15 +44,20 @@ def get_all_info_for_patientid(db, tablename, patientid):
     cur = db.cursor()
     cur.execute(f"SELECT * FROM {tablename} WHERE PatientID=?", (patientid,))
     rows = cur.fetchall()
-    print(rows)
     return rows
 
 def get_entrytype_info_for_patientid(db, tablename, patientid, entrytype):
     cur = db.cursor()
     cur.execute(f"SELECT * FROM {tablename} WHERE PatientID=? AND Type=?", (patientid,entrytype,))
     rows = cur.fetchall()
-    print(rows)
     return rows
+
+def delete_patient(db, patientid):
+    cur = db.cursor()
+    cur.execute(f"DELETE FROM Data WHERE PatientID=?", (patientid,))
+    cur.execute(f"DELETE FROM Alerts WHERE PatientID=?", (patientid,))
+    db.commit()
+    print(f"Patient {patientid} has been deleted")
 
 def print_all_tables(db):
     # List all entries in the tables 'Data' and 'Alerts'
@@ -67,17 +72,31 @@ def close_database(db):
 
 if __name__ == '__main__':
     print("Initializing Main Function . . .")
+
+    # Connect to database.db or create if not present
     db = connect_database()
+
+    # Create tables Data and Alerts if not present
     create_tables(db)
-    # add_data_item(db, '1141', '2020-03-14 14:09:10.131', 'O', '0.97')
-    # add_data_item(db, '1141', '2020-03-14 14:10:10.131', 'BP', '120/80')
-    # add_data_item(db, '1141', '2020-03-14 14:11:10.131', 'P', '90')
 
-    # add_alerts_item(db, '1141', '2020-03-14 14:09:10.131', 'O', '0.96', '0.97')
-    
-    # print_all_tables(db)
+    # Add data and alert items for Patient ID '1141'
+    add_data_item(db, '1141', '2020-03-14 14:09:10.131', 'O', '0.97')
+    add_data_item(db, '1141', '2020-03-14 14:10:10.131', 'BP', '120/80')
+    add_data_item(db, '1141', '2020-03-14 14:11:10.131', 'P', '90')
+    add_alerts_item(db, '1141', '2020-03-14 14:09:10.131', 'O', '0.96', '0.97')
 
-    get_all_info_for_patientid(db, 'Data', '1141')
-    get_entrytype_info_for_patientid(db, 'Data', '1141', 'O')
-    
+    # Get all info from table Data on Patient '1141' and print out to terminal
+    all_info = get_all_info_for_patientid(db, 'Data', '1141')
+    oxygen_info = get_entrytype_info_for_patientid(db, 'Data', '1141', 'O')
+    print(f"\nAll info for Patient '1141': {all_info}\n")
+    print(f"Oxygen info for Patient '1141': {oxygen_info}\n")
+
+    # Delete all information about Patient '1141'
+    delete_patient(db, '1141')
+
+    # Reprint information to confirm all info deleted for Patient '1141'
+    all_info = get_all_info_for_patientid(db, 'Data', '1141')
+    print(f"All info for Patient '1141': {all_info}\n")
+
+    # Close the database once finished with all tasks
     close_database(db)
